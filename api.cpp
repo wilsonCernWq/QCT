8b16431775bd77bd44bd2b6bc63cf218db632db8
Qi <qwu@sci.utah.edu>
2018-04-16 17:10:49 -0600

    make qct a libray taht can be linked against

--- api.cpp
+++ api.cpp
@@ -1,1 +1,66 @@
 #include "api.h"
+
+#include "algorithms/visit/CompositorVisIt.h"
+#include "algorithms/tree/CompositorTree.h"
+
+namespace QCT {
+
+  //! create compositor
+  void Init(const int argc, const char** argv) {};
+  Context Create(const Algorithms algo, 
+                 const size_t& w,
+                 const size_t& h)
+  {   
+    /* namescope */
+    using namespace algorithms;
+    /* create a compositor */
+    Context compositor;
+    switch (algo) {
+    case ALGO_VISIT_ICET:
+      compositor = 
+        std::make_unique<algorithms::visit::Compositor_VisIt>
+        (algorithms::visit::Compositor_VisIt::ICET, w, h);
+      break;
+    case ALGO_VISIT_ONE_NODE:
+      compositor =
+        std::make_unique<algorithms::visit::Compositor_VisIt>
+        (algorithms::visit::Compositor_VisIt::ONENODE, w, h);
+      break;
+    case ALGO_TREE:
+      throw std::runtime_error("tree algorithm not implemented");
+      break;
+    }
+    return compositor;
+  };
+  
+  //! status
+  bool IsValid(const Context& c) {
+    return c->IsValid();
+  };
+
+  //! function to get final results
+  const void *MapDepthBuffer(const Context& c) { 
+    return c->MapDepthBuffer(); 
+  };
+  const void *MapColorBuffer(const Context& c) {
+    return c->MapColorBuffer(); 
+  };
+  void Unmap(const Context& c, const void *mappedMem) {
+    c->Unmap(mappedMem);
+  };
+  
+  //! upload tile
+  void SetTile(Context& c, Tile &tile) { c->SetTile(tile); };
+  
+  //! clear (the specified channels of) this frame buffer
+  void Clear(Context& c, const uint32_t channelFlags) {
+    c->Clear(channelFlags);
+  };
+  
+  //! begin frame
+  void BeginFrame(Context& c) { c->BeginFrame(); };
+  
+  //! end frame
+  void EndFrame(Context& c) { c->EndFrame(); };
+
+};
--- api.h
+++ api.h
@@ -1,7 +1,43 @@
 #pragma once
 
-// build a binary tree compositing program step by step
+#include "algorithms/Tile.h"
 #include "algorithms/Compositor.h"
-#include "algorithms/visit/CompositorVisIt.h"
-#include "algorithms/tree/CompositorTree.h"
+#include <memory>
 
+namespace QCT {
+
+  using Context = std::unique_ptr<QCT::algorithms::Compositor>;
+
+  enum Algorithms { 
+    ALGO_VISIT_ICET, 
+    ALGO_VISIT_ONE_NODE, 
+    ALGO_TREE,
+  };
+
+  //! create compositor
+  void Init(const int argc, const char** argv);
+  Context Create(const Algorithms, 
+                 const size_t& w,
+                 const size_t& h);
+  
+  //! status
+  bool IsValid(const Context&);
+
+  //! function to get final results
+  const void *MapDepthBuffer(const Context&);
+  const void *MapColorBuffer(const Context&);
+  void Unmap(const Context&, const void *mappedMem);
+  
+  //! upload tile
+  void SetTile(Context&, Tile &tile);
+  
+  //! clear (the specified channels of) this frame buffer
+  void Clear(Context&, const uint32_t channelFlags);
+  
+  //! begin frame
+  void BeginFrame(Context&);
+  
+  //! end frame
+  void EndFrame(Context&);
+ 
+};
