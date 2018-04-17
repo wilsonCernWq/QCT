c2caccd4ea4b629ea262ade6f726f18b3aba94f6
orpheus92 <dwang0127@gmail.com>
2018-04-17 13:24:35 -0600

    Update for loading input and saving output, ready for testing

--- algorithms/tree/graph/optimize/README.md
+++ algorithms/tree/graph/optimize/README.md
@@ -4,6 +4,6 @@
 Command line programs
 
 ### create list of composition order 
-creates compositing list for nodes stored in csvfile
+reading/creating compositing list for nodes stored in csvfile
 
-`python create_tree.py [-n <randomly generated number of images>] [-o <output file>] [-h | --help]`
+`python create_tree.py [-n <randomly generated number of images, used for creating toydata>] [-o <output file>] [-i <input file>] [-h | --help]`
--- algorithms/tree/graph/optimize/create_tree.py
+++ algorithms/tree/graph/optimize/create_tree.py
@@ -1,16 +1,58 @@
 
-def addNode(Nodes, nodeval=None, imageDepth=None, halfrackNum=None, rackID=None):
+# Used for getting input data from a file or a list
+def parseNode(node):
+    # node = {"ID": "C" + '{:03}'.format(rackID) + "-" + '{:02}'.format(halfrackNum) + str(nodeval), "IMG": {}};
+    # node["IMG"]["data"] = {}
+    # node["IMG"]["depth"] = imageDepth
+    # Nodes.append(node)
+    # input.append(['{:03}'.format(rackID), '{:02}'.format(halfrackNum) + str(nodeval), str(imageDepth)])
+    if isinstance(node[0], int):
+        return 'C' + '{:03}'.format(node[0]) + '-' + '{:03}'.format(node[1])
+    else:
+        return 'C' + node[0] + '-' + node[1]
+    # return "C" + '{:03}'.format(rackID) + "-" + '{:02}'.format(halfrackNum) + str(nodeval)
+
+def pre_processing2(joblist=None, inputfile=None):
+    import csv
+    nodes = []
+
+    NodeMap = {}
+    Groups = {}
+    RankMap = {}
+    if inputfile is not None:
+        with open('input.csv') as f:
+            f_csv = csv.reader(f)
+            input = [row for row in f_csv]
+    elif joblist is not None:
+        input = joblist
+    else:
+        return [NodeMap, Groups, RankMap]
+    for index, rank in enumerate(input):
+        NID = parseNode(rank)
+        NodeMap[rank[2]] = NID
+        RankMap[NID] = str(index)
+        group = NID[0:4] + str(int(int(NID[5:7]) > 6))
+        if group not in Groups:
+            Groups[group] = 0
+        else:
+            Groups[group] = Groups[group] + 1
+
+    return [NodeMap, Groups, RankMap]
+
+
+
+
+# Used to create toy data
+
+image_num = 64
+
+def addNode(Nodes, nodeval=None, imageDepth=None, halfrackNum=None, rackID=None):  # , input=None):
     node = {"ID": "C" + '{:03}'.format(rackID) + "-" + '{:02}'.format(halfrackNum) + str(nodeval), "IMG": {}};
     node["IMG"]["data"] = {}
     node["IMG"]["depth"] = imageDepth
     Nodes.append(node)
+    input.append(['{:03}'.format(rackID), '{:02}'.format(halfrackNum) + str(nodeval), str(imageDepth)])
     return node["ID"]
-
-
-image_num = 64
-
-def pre_processing2(joblist):
-    node_num = len(joblist)/2
 
 def pre_processing(image_num):
     import random
@@ -22,15 +64,23 @@ def pre_processing(image_num):
     Groups = {}
     RankMap = {}
     d = 0
+    # Input = []
     rack_num = math.ceil(image_num / 56)
     for r in range(rack_num):
         cur_rack = random.randint(0, 999)
         for i in range(14):
             for j in range(4):
                 if d >= image_num:
+                    # import csv
+                    # tlist = list(map(list, zip(*Input)))
+                    # with open('input.csv', "w", newline='') as f:
+                    #    writer = csv.writer(f)
+                    #    writer.writerows(Input)
+
                     return [NodeMap, Groups, RankMap]
                 else:
-                    NID = addNode(nodes, nodeval=j, imageDepth=depths[d], halfrackNum=i, rackID=cur_rack)
+                    NID = addNode(nodes, nodeval=j, imageDepth=depths[d], halfrackNum=i,
+                                  rackID=cur_rack)  # , input=Input)
                     NodeMap[str(depths[d])] = NID
                     RankMap[NID] = d
                     group = NID[0:4] + str(int(int(NID[5:7]) > 6))
@@ -52,8 +102,9 @@ def pre_processing(image_num):
     #        d = d + 1
 
 
+# General use
 
-
+# Check whether a node is in base_rack
 def check_rack(NID, base_rack):
     group = NID[0:4] + str(int(int(NID[5:7]) > 6))
     if group == base_rack:
@@ -61,16 +112,16 @@ def check_rack(NID, base_rack):
     else:
         return False
 
-
+# Find base half rack to send data to
 def find_base(Groups):
     import operator
     base_rack = max(Groups.items(), key=operator.itemgetter(1))[0]
     return base_rack
 
-
+# Calculate Merge Order
 def merge(NodeMap, base_rack):
     mergelist = [[], [], []]
-    mykeys = list(NodeMap.keys()) #list(range(len(NodeMap)))
+    mykeys = list(NodeMap.keys())  # list(range(len(NodeMap)))
     new_ind = len(NodeMap)
     while len(mykeys) > 1:
         mykeys = list(NodeMap.keys())
@@ -87,7 +138,7 @@ def merge(NodeMap, base_rack):
                         mergelist[0].extend([NID, NID2])
                         mergelist[1].extend([NID2, NID])
 
-                        #mergelist[1].extend([-1, new_ind])
+                        # mergelist[1].extend([-1, new_ind])
                         mergelist[2].extend([-1, -2])
                         new_ind = new_ind + 1
                         cur_key[index] = False
@@ -99,7 +150,7 @@ def merge(NodeMap, base_rack):
                             cur_key[index + 1] is True:
                         mergelist[0].extend([NID, NID2])
                         mergelist[1].extend([NID2, NID])
-                        #mergelist[1].extend([new_ind, -1])
+                        # mergelist[1].extend([new_ind, -1])
                         mergelist[2].extend([-2, -1])
                         new_ind = new_ind + 1
                         cur_key[index] = False
@@ -125,7 +176,7 @@ def merge(NodeMap, base_rack):
                         mergelist[0].extend([NID, NID2])
 
                         mergelist[1].extend([NID2, NID])
-                        #mergelist[1].extend([-1, new_ind])
+                        # mergelist[1].extend([-1, new_ind])
                         mergelist[2].extend([-1, -2])
                         new_ind = new_ind + 1
                         cur_key[index] = False
@@ -136,45 +187,51 @@ def merge(NodeMap, base_rack):
     return mergelist
     # print(len(NodeMap))
 
+# Convert to NID to rank_number
+def post(mergelist, RankMap):  # , NodeMap):
+    for i in range(len(mergelist[0])):
+        mergelist[0][i] = RankMap[mergelist[0][i]]
+        mergelist[1][i] = RankMap[mergelist[1][i]]
 
-def post(mergelist, RankMap, NodeMap):
-    for index, item in enumerate(mergelist[0]):
-        mergelist[0][index] = RankMap[item]
-        mergelist[1][index] = RankMap[mergelist[1][index]]
+    # for index, item in enumerate(mergelist[0]):
+    #    mergelist[0][index] = RankMap[item]
+    #    mergelist[1][index] = RankMap[mergelist[1][index]]
 
     for index, item in enumerate(mergelist[2]):
-        if item == -2 and index+1<len(mergelist[0]):
-            ind = mergelist[0][index+1::].index(mergelist[0][index])
-            mergelist[2][index] = ind+index+1
-        #elif item == -2 and index % 2 == 0 and index+1<len(mergelist[0]):
+        if item == -2 and index + 1 < len(mergelist[0]):
+            ind = mergelist[0][index + 1::].index(mergelist[0][index])
+            mergelist[2][index] = ind + index + 1
+
+        # elif item == -2 and index % 2 == 0 and index+1<len(mergelist[0]):
         #    ind = mergelist[0][index+1::].index(item)
         #    mergelist[2][index] = ind+index+1
 
     # Sort first
-    #rank_num = len(RankMap)
-    #depths = list(NodeMap.keys()).map()
-    #inv_map = {k: RankMap[v] for k, v in NodeMap.items()}
+    # rank_num = len(RankMap)
+    # depths = list(NodeMap.keys()).map()
+    # inv_map = {k: RankMap[v] for k, v in NodeMap.items()}
 
-    #outlist = []
+    # outlist = []
 
-    #X = mergelist[0][0:rank_num]
-    #Y = mergelist[1][0:rank_num]
+    # X = mergelist[0][0:rank_num]
+    # Y = mergelist[1][0:rank_num]
 
-    #Y2 = [x for _, x in sorted(zip(X, Y), key=lambda pair: RankMap[pair[0]])]
-    #X2 = [x for x, _ in sorted(zip(X, Y), key=lambda pair: RankMap[pair[0]])]
+    # Y2 = [x for _, x in sorted(zip(X, Y), key=lambda pair: RankMap[pair[0]])]
+    # X2 = [x for x, _ in sorted(zip(X, Y), key=lambda pair: RankMap[pair[0]])]
 
-    #Rsort = sorted(R2sort, key=lambda nid: inv_map[nid])
-    #print(X2+mergelist[0][rank_num::])
-    #outlist.append(X2+mergelist[0][rank_num::])
-    #outlist.append(Y2+mergelist[1][rank_num::])
+    # Rsort = sorted(R2sort, key=lambda nid: inv_map[nid])
+    # print(X2+mergelist[0][rank_num::])
+    # outlist.append(X2+mergelist[0][rank_num::])
+    # outlist.append(Y2+mergelist[1][rank_num::])
 
     # NID to Rank_Num
-    #print(outlist)
-    #for index, NID in enumerate(outlist[0]):
+    # print(outlist)
+    # for index, NID in enumerate(outlist[0]):
     #    outlist[0][index] = RankMap[NID]
 
-    #return outlist
+    # return outlist
 
+# CLI
 def create():
     import argparse
     import csv
@@ -182,21 +239,27 @@ def create():
     p.add_argument('-n', '--num', default=64, type=int, dest='num', help='Number of Images')
     # p.add_argument('--csv', help='save as csv file')
     p.add_argument('-o', '--out', help='output file')
+    p.add_argument('-i', '--input', help='input file')
 
     ns = p.parse_args()
 
     image_num = int(ns.num)
-    [NodeMap, Groups, RankMap] = pre_processing(image_num)
-    NodeMap2 = {**NodeMap}
-    #print(NodeMap)
-    #print(RankMap)
+
+    if ns.input is not None:
+        [NodeMap, Groups, RankMap] = pre_processing2(inputfile=ns.input)
+    else:
+        [NodeMap, Groups, RankMap] = pre_processing(image_num)
+
+    # NodeMap2 = {**NodeMap}
+    # print(NodeMap)
+    # print(RankMap)
     base_rack = find_base(Groups)
     mergelist = merge(NodeMap, base_rack)
-    #print(mergelist)
-    #mlist = post(mergelist,RankMap, NodeMap2)
-    post(mergelist, RankMap, NodeMap2)
+    # print(mergelist)
+    # mlist = post(mergelist,RankMap, NodeMap2)
+    post(mergelist, RankMap)
 
-    print(mergelist)
+    #print(mergelist)
     tlist = list(map(list, zip(*mergelist)))
 
     if ns.out is not None:
